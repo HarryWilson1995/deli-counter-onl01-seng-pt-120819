fc76e61f47ede70502ddb1582fbcc1aca7bf6655
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 14:36:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,11 @@ def line(katz_deli, name = "")
   if katz_deli.length <= 1 
     puts "The line is currently empty."
   else 
+    katz_deli.each.each_with_index do |customer, index|
+      line_number = index + 1 
+    end 
+    puts "The line is currently: #{line_number}. #{customer} "
     katz_deli << name 
-    puts "The line is currently: "
   end 
 end 
     
