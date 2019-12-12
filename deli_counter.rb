d29b8983df540518bf2525348c2ebedbd58379d1
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 14:25:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(array, name)
+  if array.length = 0 
+    return "The line is currently empty"
+  else 
+    array << name 
+    return "Welcome, #{name}. You are number #{array.length} in line."
+  end 
+end 
+    
+  
