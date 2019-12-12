9e0fed6fe0fb0597cf5d413cdbb1fc561090d1ff
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 14:30:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 katz_deli = []
 
-def line(array, name = "")
-  if !array.length
+def line(katz_deli, name = "")
+  if !katz_deli.length
     puts "The line is currently empty."
   else 
-    array << name 
-    puts "Welcome, #{name}. You are number #{array.length} in line."
+    katz_deli << name 
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end 
 end 
     
